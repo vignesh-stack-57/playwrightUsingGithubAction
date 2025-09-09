e875c1f7de876cb0afef27dad8777428951dc61c
vignesh-stack-57 <vigneshsugumar2002@gmail.com>
2025-09-10 01:48:35 +0530

    updated script make an error  file

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://www.npmjs.com/package/playwright');
   await page.locator('#maindafsfdsdsa').click();
+
+  
   await page.getByRole('tab', { name: 'Depenzcxzcdscdsdency' }).click();
   await page.getByRole('heading', { name: 'Depexzvxvvcxzndencies (1)' }).click();
   await page.getByRole('combobox', { name: 'Searcxzcdvh packages' }).click();
